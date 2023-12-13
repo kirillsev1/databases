90ca88698ee11d277e7ebb113880db1ae5b49278
kirillsev1 <kirillprih47@gmail.com>
2023-12-14 00:22:34 +0300

    added fuzzy matching search

--- app.js
+++ app.js
@@ -39,6 +39,35 @@ app.get('/explorer', async (req, res) => {
     }
 })
 
+app.get('/expedition', async (req, res) => {
+    try {
+        const name = req.query['name']
+        var result = ''
+        if (name == null) {
+            result = await client.search({index: 'expedition'})
+        }
+        else {
+            result = await client.search({
+                index: 'expedition',
+                query: {
+                    match: {
+                        name: {
+                            query : name,
+                            fuzziness: 1,
+                            minimum_should_match: 2
+                        }
+                    }
+                }
+            })
+        }
+
+        res.json(result.hits.hits)
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(400)
+    }
+})
+
 app.listen(appPort, () => {
     console.log(`app listening on port ${appPort}`)
 })
