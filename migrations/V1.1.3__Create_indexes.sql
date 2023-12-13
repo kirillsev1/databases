1891994c408931784dd871cb4e47982dfd1856e0
kirillsev1 <kirillprih47@gmail.com>
2023-12-13 15:36:03 +0300

    rm files

--- migrations/V1.1.3__Create_indexes.sql
+++
@@ -1,3 +0,0 @@
-create index expedition_start_date on expedition using btree(start_date);
-create index expedition_start_date_expedition_result on expedition using btree(start_date, expedition_result);
-create index expedition_name on expedition using hash(name);
--- migrations/V1.1.4__Create_gin_index.sql
+++
@@ -1,3 +0,0 @@
-create extension if not exists pg_trgm;
-create index ngram_text_search on expedition using gin(name gin_trgm_ops);
-create extension if not exists fuzzystrmatch;
